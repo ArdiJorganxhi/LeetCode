94c07a3a74bb1e470c1bad99605f892656aa504f
Ardi Jorganxhi <ardijorganxhi@eskisehir.edu.tr>
2024-03-23 01:06:04 +0100

    two sum problem

---
+++ src/TwoSum/Solution.java
@@ -0,0 +1,14 @@
+package TwoSum;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i=1; i < nums.length; i++) {
+            for (int j=i; j < nums.length; j++) {
+                if (nums[j]+nums[j - i] == target){
+                    return new int[] {j, j - i};
+                }
+            }
+        }
+        return null;
+    }
+}
