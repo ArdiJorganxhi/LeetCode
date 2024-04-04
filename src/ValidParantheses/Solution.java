072b05d7ed57b532bf432d0ab00468f2db681126
Ardi Jorganxhi <ardijorganxhi@eskisehir.edu.tr>
2024-04-05 01:24:04 +0200

    valid paranthesis solution

---
+++ src/ValidParantheses/Solution.java
@@ -0,0 +1,30 @@
+package ValidParantheses;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+import java.util.Stack;
+
+class Solution {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (final char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+       boolean result = isValid("()");
+        System.out.println(result);
+    }
+}
