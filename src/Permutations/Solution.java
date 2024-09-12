ab28c286083baa429ae433630f5585059bf27316
Ardi Jorganxhi <ardijorganxhiu@gmail.com>
2024-09-13 00:08:52 +0200

    implement solution for permutations problem

---
+++ src/Permutations/Solution.java
@@ -0,0 +1,31 @@
+package Permutations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), nums);
+        return result;
+    }
+
+    private static void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums) {
+        if (tempList.size() == nums.length) {
+            result.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (tempList.contains(nums[i])) continue;
+                tempList.add(nums[i]);
+                backtrack(result, tempList, nums);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3};
+        System.out.println(permute(nums1));
+    }
+}
